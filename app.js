a8c99b75a7b1c10ec6534144e40056527b242ff0
denisramosesti-ung <denisramosesti@gmail.com>
2025-09-22 15:32:09 -0300

    Prueba de CI/CD

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT  8080;
 
 app.get(, (req, res) = {
-  res.send(¡Hola Mundo desde GCP con CICD!);
+  res.send(¡Hola Mundo desde GCP con CICD!  - Prueba de cambios);
 });
 
 app.listen(PORT, () = console.log(`Servidor corriendo en puerto ${PORT}`));
