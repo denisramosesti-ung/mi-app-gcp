545672a33719f1e7a9e59e56abb4d3f80ab6fa17
denisramosesti-ung <denisramosesti@gmail.com>
2025-09-22 15:08:35 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require(express);
+const app = express();
+const PORT = process.env.PORT  8080;
+
+app.get(, (req, res) = {
+  res.send(¡Hola Mundo desde GCP con CICD!);
+});
+
+app.listen(PORT, () = console.log(`Servidor corriendo en puerto ${PORT}`));
